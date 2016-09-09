e14245c222a66e5179d0e51a8564eecf328345f8
Elysia Nason <elysia.avery@gmail.com>
2016-09-09 10:22:19 -0700

    add working ui

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,15 +6,18 @@ public class App {
   public static void main(String[] args) {
     Console c = System.console();
     // Event halloween = new Event("Halloween Party", 250, 10, 20, 250);
-    // List<Event> allEvents = new ArrayList<Event>();
-    Event newEvent = new Event();
-    System.out.println("Welcome to Thra event planning! We can help you plan your next event or party. Please type in one of the following so that we may assist you: plan party, plan a party for me, or exit");
-    String navigationChoice = myConsole.readLine().trim().toLowerCase();
+    List<Event> allEvents = new ArrayList<Event>();
+    System.out.println("Welcome to Thra event planning! We can help you plan your next event or party. Please type in one of the following so that we may assist you: Plan Party, Plan a Party For Me, or Exit");
+    String navigationChoice = c.readLine().trim().toLowerCase();
 
     if(navigationChoice.equals("plan party")) {
       System.out.println("Let's begin planning your party! How many people are you planning on having attend?");
       int totalPeopleSelection = Integer.parseInt(c.readLine());
+      System.out.println("What type of meal would you like to be served? Please type one of the following: Nebrie Grub Cheese and Plant Tendrils, urAmaj and urNol's Healing Feast, or skekMal's Freshly Hunted Meats");
+      int userMealChoice = Integer.parseInt(c.readLine().trim().toLowerCase());
+      System.out.println("What type of entertainment would you like at your event?");
+      int userChoiceEntertainment = Integer.parseInt(c.readLine().trim().toLowerCase());
+      Event userEvent = new Event(totalPeopleSelection, userMealChoice, userChoiceEntertainment);
     }
-
   }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,37 +1,61 @@
-
-public class Event {
-  private String mPartyName;
-  private int mBase = 0;
+class Event {
   private int mCost = 250;
-  private int mPerson = 15;
-  private int mMeal = 25;
-  private int mEntertainment = 250;
+  private int mPerson;
+  private int mMealType;
+  private int mEntertainment;
 
-  // public Event(String partyName, int cost, int person, int meal, int entertainment) {
-  //   mPartyName = partyName;
-  //   mCost = cost;
-  //   mPerson = person;
-  //   mMeal = meal;
-  //   mEntertainment = entertainment;
-  // }
-
-  public int initialCost(int userChoice) {
-    // return mBase.equals(mCost + userChoice);
-    return 250;
-  }
-
-  public int costPerPerson(int totalPeopleSelection) {
-    // return mBase.equals(totalPeopleSelection * mPerson);
-    return 15;
-  }
-
-  public int costPerPlate(int foodSelection) {
-    // return mBase.equals(foodSelection.mMeal * totalPeopleSelection);
-    return 25;
-  }
-
-  public int costPerEntertainment(int entertainmentSelection) {
-    // return mBase.equals(entertainmentSelection.mEntertainment);
-    return 250;
-  }
+public Event(int person, int mealType, int entertainment){
+  // mCost = cost;
+  mPerson = person;
+  mMealType = mealType;
+  mEntertainment = entertainment;
 }
+public int attendance(){
+  return (mCost + mPerson);
+}
+public int costPerMeal(){
+return (mPerson * mMealType);
+}
+}
+
+
+
+
+
+
+//
+// public class Event {
+//   private String mPartyName;
+//   private int mBase = 0;
+//   private int mCost = 250;
+//   private int mPerson = 15;
+//   private int mMeal = 0;
+//   private int mEntertainment = 250;
+//
+//   public Event(String partyName, int cost, int person, int meal, int entertainment) {
+//     mPartyName = partyName;
+//     mCost = cost;
+//     mPerson = person;
+//     mMeal = meal;
+//     mEntertainment = entertainment;
+//   }
+//
+//   public int initialCost(int userChoice) {
+//     // return mBase.equals(mCost + userChoice);
+//     return 250;
+//   }
+//
+//   public int costPerPerson(int totalPeopleSelection) {
+//     // return mBase += totalPeopleSelection * mPerson;
+//     return 15;
+//   }
+//
+//   public int costPerPlate() {
+//     return 25;
+//   }
+//
+//   public int costPerEntertainment(int entertainmentSelection) {
+//     // return mBase.equals(entertainmentSelection.mEntertainment);
+//     return 250;
+//   }
+// }
